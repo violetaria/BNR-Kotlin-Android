9e443fd2205a2a9f703cfad3d09345c2f0c26744
Terri Chu <terri.chu.dev@gmail.com>
2018-11-25 19:29:12 -0500

    working app!

---
+++ app/src/main/java/com/getlosthere/samodelkin/NewCharacterActivity.kt
@@ -0,0 +1,52 @@
+package com.getlosthere.samodelkin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_new_character.*
+
+private const val CHARACTER_DATA_KEY = "CHARACTER_DATA_KEY"
+
+private var Bundle.characterData
+    get() = getSerializable(CHARACTER_DATA_KEY) as
+            CharacterGenerator.CharacterData
+    set(value) = putSerializable(CHARACTER_DATA_KEY, value)
+
+class NewCharacterActivity : AppCompatActivity() {
+    private var characterData = CharacterGenerator.generate()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_character)
+
+//        characterData = savedInstanceState?.let {
+//            it.getSerializable(CHARACTER_DATA_KEY)
+//                    as CharacterGenerator.CharacterData
+//        } ?: CharacterGenerator.generate()
+
+        characterData = savedInstanceState?.characterData ?:
+                CharacterGenerator.generate()
+
+        generateButton.setOnClickListener {
+            characterData = CharacterGenerator.generate()
+            displayCharacterData()
+        }
+
+       displayCharacterData()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+//        outState.putSerializable(CHARACTER_DATA_KEY, characterData)
+        outState.characterData = characterData
+    }
+
+    private fun displayCharacterData() {
+        characterData.run {
+            nameTextView.text = name
+            raceTextView.text = race
+            dexterityTextView.text = dex
+            wisdomTextView.text = wis
+            strengthTextView.text = str
+        }
+    }
+}
